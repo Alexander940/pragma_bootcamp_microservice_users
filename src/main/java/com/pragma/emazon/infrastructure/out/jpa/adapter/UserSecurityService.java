ce27716d6e3d7566deef5aba411e77b5066fa0fc
Alexander940 <alexanderecheverry@outlook.es>
2024-08-28 18:26:41 -0500

    UserSecurityService implemented

---
+++ src/main/java/com/pragma/emazon/infrastructure/out/jpa/adapter/UserSecurityService.java
@@ -0,0 +1,31 @@
+package com.pragma.emazon.infrastructure.out.jpa.adapter;
+
+import com.pragma.emazon.infrastructure.out.jpa.entity.UserEntity;
+import com.pragma.emazon.infrastructure.out.jpa.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found."));
+
+        return User.builder()
+                .username(userEntity.getEmail())
+                .password(userEntity.getPassword())
+                .roles(userEntity.getRole().getName().toUpperCase())
+                .build();
+    }
+}
