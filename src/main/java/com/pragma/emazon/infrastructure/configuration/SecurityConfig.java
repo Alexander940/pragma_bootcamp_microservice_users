7dcb74c860ae90e4eae1c886d1765b27223b7e03
Alexander940 <alexanderecheverry@outlook.es>
2024-08-28 15:03:33 -0500

    security config initialized

---
+++ src/main/java/com/pragma/emazon/infrastructure/configuration/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.pragma.emazon.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
