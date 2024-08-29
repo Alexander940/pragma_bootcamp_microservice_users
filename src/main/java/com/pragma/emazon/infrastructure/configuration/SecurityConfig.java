23da988e4220a35c602bdf749eae885965e69e1d
Alexander940 <alexanderecheverry@outlook.es>
2024-08-28 21:57:56 -0500

    ApplicationTest fixed, bcrypt add in SecurityConfig

--- src/main/java/com/pragma/emazon/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/com/pragma/emazon/infrastructure/configuration/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.pragma.emazon.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +16,16 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/test/java/com/pragma/emazon/EmazonMicroserviceUserApplicationTests.java
+++ src/test/java/com/pragma/emazon/EmazonMicroserviceUserApplicationTests.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/test/java/com/pragma/emazon/EmazonMicroserviceUserApplicationTests.java
 package com.pragma.emazon;
-========
-package com.pragma;
->>>>>>>> master:src/test/java/com/pragma/EmazonMicroserviceUserApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
